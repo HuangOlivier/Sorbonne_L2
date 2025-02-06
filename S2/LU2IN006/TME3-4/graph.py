ad779bb30e7832f3492cac1e0bbe112b1255d0f4
Andre Bertok <Andre.Bertok@etu.sorbonne-universite.fr>
2025-02-06 23:32:22 +0100

    :construction: SDD code fini (pre opti et clean)

---
+++ S2/LU2IN006/TME3-4/graph.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Charger le fichier CSV
+data = pd.read_csv("data.csv")
+
+# Extraire les colonnes
+tailles = data["Taille"]
+temps_liste = data["Temps_Liste"]
+temps_hash = data["Temps_Hash"]
+
+# Créer le graphique
+plt.figure(figsize=(10, 6))
+plt.plot(tailles, temps_liste, marker='o', linestyle='-', color='red', label="Temps de recherche (Liste)")
+plt.plot(tailles, temps_hash, marker='s', linestyle='-', color='blue', label="Temps de recherche (Table de hachage)")
+
+# Ajouter des étiquettes et un titre
+plt.xlabel("Nombre de livres")
+plt.ylabel("Temps de recherche (secondes)")
+plt.title("Temps de recherche en fonction de la taille de la bibliothèque")
+plt.legend()
+plt.grid(True)
+
+# Sauvegarder le graphique en image
+plt.savefig("temps_recherche_bibliotheque.png", dpi=300)
+plt.show()
--- S2/LU2IN006/TME3-4/main.c
+++ S2/LU2IN006/TME3-4/main.c
@@ -4,7 +4,7 @@
 #include "biblioH.h"
 
 #include <stdlib.h>
-
+#include <time.h>
 
 void menu() {
 	printf("Option:\n");
@@ -20,9 +20,112 @@ int main(int argc, char** argv) {
 		return -1;
 	};
 
-	Biblio* LinkedList = charger_n_entrees(argv[1], atoi(argv[2]));
-	BiblioH* hash = charger_n_entreesH(argv[1], atoi(argv[2]));
+	//Biblio* LinkedList = charger_n_entrees(argv[1], atoi(argv[2]));
+	//BiblioH* hash = charger_n_entreesH(argv[1], atoi(argv[2]));
 
+
+	clock_t temps_init;
+	clock_t temps_final;
+	double temps_cpu;
+
+
+
+	/*
+	int randomN = rand() % atoi(argv[2]);
+	temps_init = clock();
+	for(int i=0; i<150;i++) {
+		recherche1(LinkedList->L, randomN);
+	}
+	temps_final=clock();
+	temps_cpu = ((double)(temps_final - temps_init)) / CLOCKS_PER_SEC;
+	printf("Liste chainée: recherche 1 - %f \n", temps_cpu);
+
+
+	temps_init = clock();
+	for(int i=0; i<150;i++) {
+		recherche1H(hash, randomN);
+	}
+	temps_final=clock();
+	temps_cpu = ((double)(temps_final - temps_init)) / CLOCKS_PER_SEC;
+	printf("Table de hachage: recherche 1 - %f \n", temps_cpu);
+
+	printf("\n\n\n");
+
+	temps_init = clock();
+	for(int i=0; i<150;i++) {
+		recherche2(LinkedList->L, "YPQRXWOAWBMSYX");
+	}
+	temps_final=clock();
+	temps_cpu = ((double)(temps_final - temps_init)) / CLOCKS_PER_SEC;
+	printf("Liste chainée: recherche 2 - %f \n", temps_cpu);
+
+	temps_init = clock();
+	for(int i=0; i<150;i++) {
+		recherche2H(hash, "YPQRXWOAWBMSYX");
+	}
+	temps_final=clock();
+	temps_cpu = ((double)(temps_final - temps_init)) / CLOCKS_PER_SEC;
+	printf("Table de hachage: recherche 2 - %f \n", temps_cpu);
+
+
+	printf("\n\n\n");
+
+
+	temps_init = clock();
+	for(int i=0; i<300;i++) {
+		recherche3(LinkedList->L, "exqb");
+	}
+	temps_final=clock();
+	temps_cpu = ((double)(temps_final - temps_init)) / CLOCKS_PER_SEC;
+	printf("Liste chainée: recherche 3 - %f \n", temps_cpu);
+
+	temps_init = clock();
+	for(int i=0; i<300;i++) {
+		recherche3H(hash, "exqb");
+	}
+	temps_final=clock();
+	temps_cpu = ((double)(temps_final - temps_init)) / CLOCKS_PER_SEC;
+	printf("Table de hachage: recherche 3 - %f \n", temps_cpu);
+
+
+	printf("\n\n\n");
+
+
+	
+	*/
+	FILE *f = fopen("data.csv", "w");
+	fprintf(f,"Taille,Temps_Liste,Temps_Hash\n");
+
+	for (int n=10; n<=100000; n=n*1.1){
+		Biblio* LinkedList = charger_n_entrees(argv[1],n);
+		BiblioH* hash = charger_n_entreesH(argv[1], n);
+		double temps_cpuL;
+		double temps_cpuH;
+
+		temps_init = clock();
+		recherche4(LinkedList);
+		temps_final=clock();
+		temps_cpuL = ((double)(temps_final - temps_init)) / CLOCKS_PER_SEC;
+		//printf("Liste chainée: recherche 4 - %f \n", temps_cpuL);
+
+
+		temps_init = clock();
+		recherche4H(hash);
+		temps_final=clock();
+		temps_cpuH = ((double)(temps_final - temps_init)) / CLOCKS_PER_SEC;
+		//printf("Table de hachage: recherche 4 - %f \n", temps_cpuH);
+		printf("%d \n", n);
+		fprintf(f,"%d,%f,%f\n",n, temps_cpuL, temps_cpuH);
+
+
+		liberer_biblio(LinkedList);
+		liberer_biblioH(hash);
+	}
+
+	fclose(f);
+
+	
+	/*
 	//afficher_biblioH(hash);
 	BiblioH* t = recherche3H(hash, "xyrivhpp");
 	BiblioH* c = recherche3H(hash, "bbqcwi");
@@ -33,11 +136,9 @@ int main(int argc, char** argv) {
 
 	afficher_biblioH(dup);
 	
-	liberer_biblio(LinkedList);
 	
-	liberer_biblioH(hash);
 	liberer_biblioH(fusion);
-	
+	*/
 	
 	/*
 	int rep;
