bc76c112dbb5b95922020fdae50884c4e79a61da
Olivier Huang <81701540+HuangOlivier@users.noreply.github.com>
2025-03-12 22:26:13 +0100

    Update hash.c
    accepte pas juste pour te montrer vite fais mais j'ai essayer de regler les erreur de compile parci parla mais c'est surement faux

--- S2/LU2IN006/projet/hash.c
+++ S2/LU2IN006/projet/hash.c
@@ -1,7 +1,7 @@
 #include "hash.h"
 #include <stdio.h>
-
-
+#include <stdlib.h>
+#include <string.h>
 unsigned long simple_hash(const char *str) {
 	unsigned long res=0;
 	
@@ -20,11 +20,10 @@ int h(const char *str, int i) {
 HashMap *hashmap_create() {
 	HashMap *new = malloc(sizeof(HashMap));
 	new->size = TABLE_SIZE;
-	new->current_mem = 0;
-	new->table = malloc(sizeof(HashEntry*) * TABLE_SIZE);
-	
+	new->table = malloc(sizeof(HashEntry*) * TABLE_SIZE );
+
 	for (int i=0; i < TABLE_SIZE; i++) {
-		new->table[i] = NULL;
+		new->table[i].value = NULL;
 	}
 	
 	return new;
@@ -38,11 +37,12 @@ int hashmap_insert(HashMap *map, const char *key, void *value) {
 	 int i=0;
 	 unsigned long k = h(key,i);
 	 
-	 while (map->table[k] != TOMBSTONE && map->table[k] != NULl) {
+	 while ((map->table[k].value != TOMBSTONE) && (map->table[k].value != NULL)) {
 	 	i++;
 	 	k = (h(key,i) % map->size); 	
 	 }
-	 map->table[k] = elem;
+	 map->table[k].key = elem->key;
+	 map->table[k].value = elem->value;
 	 return k;
 }
 
@@ -51,9 +51,9 @@ void *hashmap_get(HashMap *map, const char *key) {
 	int i=0;
 	unsigned long k = h(key,i);
 	
-	while (map->table[k] != NULL) {
-		if(strcmp(map->table[k]->key,key)==0) {
-			return map->table[k]->value;
+	while (map->table[k].value != NULL) {
+		if(strcmp(map->table[k].key,key)==0) {
+			return map->table[k].value;
 		}
 		i++;
 		k = (h(key,i) % map->size); 	
@@ -63,16 +63,22 @@ void *hashmap_get(HashMap *map, const char *key) {
 }
 
 int hashmap_remove(HashMap *map, const char *key) {
-	
+	for(int i=0;i<map->size;i++){
+			if(strcmp(map->table[i].key,key)==0){
+					map->table[i].value = TOMBSTONE;
+					return 1;
+			}
+	}
+	return 0;
 }
 
 
 void hashmap_destroy(HashMap *map) {
 	if (map) {
 		for (int i=0; i<map->size; i++) {
-			free(map->table[i]->key);
-			free(map->table[i]->value);
-			free(map->table[i]);
+			free(map->table[i].key);
+			free(map->table[i].value);
+			//free(map->table[i]);
 		}
 		free(map->table);
 		free(map);
