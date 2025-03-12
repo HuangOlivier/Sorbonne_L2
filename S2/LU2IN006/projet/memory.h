90fd6797e96724c1bba30ef8d989e079c11fe2b7
Olivier Huang <81701540+HuangOlivier@users.noreply.github.com>
2025-03-12 22:24:51 +0100

    Update memory.h
    aucun pb celui tu peux accepter

--- S2/LU2IN006/projet/memory.h
+++ S2/LU2IN006/projet/memory.h
@@ -7,7 +7,7 @@
 typedef struct segment {
 	int start;
 	int size;
-	struct Segment *next;
+	struct segment *next;
 } Segment;
 
 typedef struct memoryHandler {
@@ -15,6 +15,11 @@ typedef struct memoryHandler {
 	int total_size;
 	Segment *free_list;
 	HashMap *allocated;
-}
+}MemoryHandler;
+
+MemoryHandler *memory_init(int size);
+Segment* find_free_segment(MemoryHandler* handler, int start, int size, Segment** prev);
+int create_segment(MemoryHandler *handler, const char *name, int start, int size);
+int remove_segment(MemoryHandler *handler, const char *name);
 
 #endif
