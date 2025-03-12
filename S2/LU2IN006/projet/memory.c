bacb8f3950d6c8bf9c34d0fdab27446e958c6848
Olivier Huang <81701540+HuangOlivier@users.noreply.github.com>
2025-03-12 22:23:12 +0100

    Update memory.c
    pas complet j'ai encore des test a faire + d'autre modif

--- S2/LU2IN006/projet/memory.c
+++ S2/LU2IN006/projet/memory.c
@@ -1,12 +1,14 @@
 #include "memory.h"
-
+#include "hash.h"
+#include <stdio.h>
+#include <stdlib.h>
 MemoryHandler *memory_init(int size) {
 	MemoryHandler *new = malloc(sizeof(MemoryHandler));
-	
-	
+
 	Segment *seg = malloc(sizeof(Segment));
 	seg->start = 0;
 	seg->size = size;
+
 	seg->next = NULL;
 	
 	new->free_list = seg;
@@ -14,27 +16,23 @@ MemoryHandler *memory_init(int size) {
 	new->total_size = size;
 	
 	return new;
-	//void **memory ?
 }
 
 Segment* find_free_segment(MemoryHandler* handler, int start, int size, Segment** prev) {
 	Segment* tmp = handler->free_list;
-	Segment* prec = NULL;
 	
 	while(tmp) {
 		if (tmp->start <= start && tmp->start+tmp->size >= start+size) {
-			*prev = prec; 
 			return tmp;
 		}
-		
-		prec = tmp;
+		*prev = tmp;
 		tmp = tmp->next;
 	}
 	
 	return NULL;
 }
 
-int create_sement(MemoryHandler *handler, const char *name, int start, int size) {
+int create_segment(MemoryHandler *handler, const char *name, int start, int size) {
 	Segment *prec = NULL;
 	Segment *n = find_free_segment(handler, start, size, &prec);
 	
@@ -43,16 +41,52 @@ int create_sement(MemoryHandler *handler, const char *name, int start, int size)
 		new_seg->start = start;
 		new_seg->size = size;
 		new_seg->next = NULL;
-		
 		hashmap_insert(handler->allocated, name, (void*) new_seg);
-		
-		
-		//n->size / 2
-		//n1->start = n->start
-		//n1->size = n->start
+
+		Segment *new1 = malloc(sizeof(Segment));
+		new1->start = n->start;
+		new1->size =start - n->start; 
+		new1->next =n;
+
+		n->start =start + size;
+		n->size =n->size - size;
+
+		if (prec) {
+			prec->next = new1;
+		} else {
+			handler->free_list = new1;
+		}
 	}
-	
+	return 0;// ne sais pas quoi retourner
 }
+	
 
+int remove_segment(MemoryHandler *handler, const char *name){
+	Segment *new = hashmap_get(handler->allocated, name);
+	hashmap_remove(handler->allocated,name);
+	Segment *seg_free=handler->free_list;
+	Segment *prec;
 
+	while (seg_free && seg_free->start < new->start) {
+        prec = seg_free;
+        seg_free = seg_free->next;
+    }
+
+    if (prec && prec->start + prec->size == new->start) {
+        prec->size +=new->size;
+        free(new);
+        new = prec;
+    } else {
+        new->next = seg_free;
+        if (prec) prec->next = new;
+        else handler->free_list = new;
+    }
+
+    if (seg_free && new->start + new->size == seg_free->start) {
+        new->size += seg_free->size;
+        new->next =seg_free->next;
+        free(seg_free);
+    }
+	return 0;
+}
 
